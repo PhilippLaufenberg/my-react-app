eac7378f6eccccc87dda3bb613f990b97bfef71b
Phil Laufenberg <hello@phillaufenberg.com>
2024-03-20 05:53:02 +0100

    add simple button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
+import Button from "./Button.jsx";
 
 function App() {
   return (
@@ -9,6 +8,7 @@ function App() {
       <Card />
       <Card />
       <Card />
+      <Button />
     </>
   );
 }
---
+++ src/Button.jsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <Button className="button">Click me</Button>;
+}
+
+export default Button;
