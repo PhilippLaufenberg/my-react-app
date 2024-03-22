2cee8ce5ff908742224c1f1836721da93c716dde
Phil Laufenberg <hello@phillaufenberg.com>
2024-03-22 16:19:06 +0100

    styling the button component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,9 @@
 function Button() {
-  return <button className="button">Click me</button>;
+  return (
+    <button className="bg-blue-500 text-white py-2 px-5 rounded border-none cursor-pointer">
+      Click me
+    </button>
+  );
 }
 
 export default Button;
